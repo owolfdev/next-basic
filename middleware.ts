d989f6a675b02d4ad7f132cd74e1971543b46b8d
owolfdev <owolfdev@gmail.com>
2023-12-18 08:56:09 +0700

    add clerk, modify public routes

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 
 export default authMiddleware({
-  //   publicRoutes: ["/", "/contact"],
+  publicRoutes: ["/", "/contact"],
 });
 
 export const config = {

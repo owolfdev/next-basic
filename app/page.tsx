a3738e4b88922b6cbb9a8b001ac17f9202b281e6
owolfdev <owolfdev@gmail.com>
2023-12-19 04:37:51 +0700

    markdown added

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,30 @@
 import Image from "next/image";
+import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
 
 export default function Home() {
+  const markdown = `
+  🚀 **Next.js 14 Framework**: This is a basic template starter using Next.js 14. It offers efficient performance and fast page loading.
+
+  🌟 **Shadcn UI Elements**: The interface uses Shadcn UI components. It's designed to be responsive and user-friendly.
+  
+  🔐 **Clerk Authentication**: The template includes Clerk for secure authentication. It ensures safe and reliable user sign-in experiences.
+  
+  🛠️ **Customizable Template**: This starter template is designed for customization. It can be adapted for personal projects or business applications.
+  
+  🌍 **Community and Exploration**: Users are encouraged to engage with the community. This template serves as a starting point for development and innovation.
+  
+  🎉 **Getting Started**: Begin your development with this Next.js 14 starter template. It's a foundation for creating modern web applications.
+   
+  `;
+
   return (
     <div className="max-w-xl z-10 w-full items-center justify-between text-sm lg:flex">
       <div className="w-full flex justify-center items-center flex-col gap-6">
         <h1 className="text-5xl sm:text-6xl font-bold">Basic Next.js</h1>
-        <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
-          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
-          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
-          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
-          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
-          qui dolorem eum fugiat quo voluptas nulla pariatur?
-        </p>
+        <div className="flex flex-col gap-4 text-[16px]">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </div>
       </div>
     </div>
   );

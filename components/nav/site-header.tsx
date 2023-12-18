b759799960efcabae667aee077ca8d9449894ac9
owolfdev <owolfdev@gmail.com>
2023-12-19 04:17:16 +0700

    update

--- components/nav/site-header.tsx
+++ components/nav/site-header.tsx
@@ -12,6 +12,9 @@ function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="sm:px-12 px-6 flex items-center h-16 space-x-4 sm:justify-between sm:space-x-0">
+        <Link href="/">
+          <div className="text-4xl font-bold">Next Basic</div>
+        </Link>
         <div className="flex items-center justify-end flex-1 space-x-4">
           <div className="flex gap-4 items-center">
             <UserButton afterSignOutUrl="/" />
